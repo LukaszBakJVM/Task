d021aec09ae8dbbfd2c24f73d20e9726e71ecc3c
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-03-26 11:00:57 +0100

    Update

--- src/main/java/org/lukasz/Main.java
+++ src/main/java/org/lukasz/Main.java
@@ -1,37 +1,20 @@
 package org.lukasz;
 
-import java.util.List;
-import java.util.Optional;
-
 public class Main {
     public static void main(String[] args) {
         FolderCabinet folderCabinet = new FolderCabinet();
-        Folder folder = new FolderCabinet("aaa","bbb");
+        Folder folder = new FolderCabinet("one", "SMALL");
+        Folder folder1 = new FolderCabinet("two", "SMALL");
+        Folder folder2 = new FolderCabinet("three", "MEDIUM");
+        Folder folder3 = new FolderCabinet("three", "LARGE");
+
         folderCabinet.folderArrayList.add(folder);
+        folderCabinet.folderArrayList.add(folder1);
+        folderCabinet.folderArrayList.add(folder2);
+        folderCabinet.folderArrayList.add(folder3);
 
 
 
-        Optional<Folder> aaa = folderCabinet.findFolderByName("aaa");
-
-
-
-
-        MultiFolder multiFolder = new FolderCabinet();
-        List<Folder> folders = multiFolder.getFolders();
-       folders.add(folder);
-       FolderCabinet folderCabinet1 = new FolderCabinet(folders);
-
-        Optional<Folder> aaa1 = folderCabinet1.findFolderByName("aaa");
-
-
-
-
-
-
-
-
-
-       // System.out.println(multiFolder.getFolders().size());
 
 
 
