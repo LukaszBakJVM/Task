4505592728ef5adc641ddd4fd9c6c8129e084d5c
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-03-26 13:43:14 +0100

    refactor

--- src/main/java/org/lukasz/FileCabinet.java
+++ src/main/java/org/lukasz/FileCabinet.java
@@ -1,47 +1,25 @@
 package org.lukasz;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 public class FileCabinet implements Cabinet {
     private List<Folder> folders;
 
-
     @Override
     public Optional<Folder> findFolderByName(String name) {
-        return Optional.of(getFoldersList().getFirst());
+        return Optional.empty();
     }
 
     @Override
     public List<Folder> findFoldersBySize(String size) {
-        return getFoldersList();
+        return List.of();
     }
 
     @Override
     public int count() {
-        return getFoldersList().size();
+        return 0;
     }
-
-    public List<Folder> getFoldersList() {
-        return folders;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        FileCabinet that = (FileCabinet) o;
-        return Objects.equals(folders, that.folders);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(folders);
-    }
-
-
 }
 
 
