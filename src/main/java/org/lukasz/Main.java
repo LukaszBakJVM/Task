901fe8d597af9ac2fd06c4c7d1dc8046bcd4b33c
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-03-27 13:33:18 +0100

    refactor

--- src/main/java/org/lukasz/Main.java
+++ src/main/java/org/lukasz/Main.java
@@ -2,16 +2,7 @@ package org.lukasz;
 
 public class Main {
     public static void main(String[] args) {
-        FolderCabinet folderCabinet = new FolderCabinet();
-        Folder folder = new FolderCabinet("one", "SMALL");
-        Folder folder1 = new FolderCabinet("two", "SMALL");
-        Folder folder2 = new FolderCabinet("three", "MEDIUM");
-        Folder folder3 = new FolderCabinet("three", "LARGE");
 
-        folderCabinet.folderArrayList.add(folder);
-        folderCabinet.folderArrayList.add(folder1);
-        folderCabinet.folderArrayList.add(folder2);
-        folderCabinet.folderArrayList.add(folder3);
 
 
 
